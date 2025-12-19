e668ee864765e76bdefd18fe352ff7e1a755ad4a
lazark <lazar.knezevic@pl.abb.com>
2025-12-19 16:02:38 +0100

    langchain semantic search

--- AI/api/semantic_search.py
+++ AI/api/semantic_search.py
@@ -1,3 +1,12 @@
 from langchain_community.document_loaders  import PyPDFLoader
 
 
+file_path = "data/nke-10k-2023.pdf"
+
+class SemanticSearch:
+    def __init__(self):
+        self.loader = PyPDFLoader(file_path)
+        self.docs = self.loader.load()
+        
+    
+
